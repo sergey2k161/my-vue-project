616b3d297645240fa9bf98921ff8570ccc492f38
sergey2k161 <joysergey4@gmail.com>
2024-05-28 15:43:01 +0300

    Style TaskManager.vue

--- src/components/TaskManager.vue
+++ src/components/TaskManager.vue
@@ -1,38 +1,48 @@
 ﻿<template>
-  <div>
+  <div class="task-manager vh-100">
     <form @submit.prevent="tryAddTask">
-      <div class="toDoList">
-        <b-button variant="outline-dark" @click="toggleTaskList">Список задач</b-button>
-        <b-button @click="isTaskModalVisible = true">+</b-button>
-        <b-modal v-model="isTaskModalVisible" title="Создание задачи">
-          <div>
-            <label for="title">Название:</label>
-            <input type="text" v-model="model.title" id="title" required />
-          </div>
-          <div>
-            <label for="description">Описание:</label>
-            <textarea v-model="model.description" id="description" required></textarea>
-          </div>
-          <div>
-            <label for="deadline">Выполнить до:</label>
-            <input type="date" v-model="model.deadline" id="deadline" required />
-          </div>
-          <div>
-            <label for="taskStatus">Статус задачи:</label>
+      <div class="toDoList ">
+        <b-button class="addTaskButton w-100" @click="isTaskModalVisible = true">Создать новую задачу</b-button>
+        <b-button class="addTaskButton w-100" @click="toggleTaskList">Список задач</b-button>
+        <b-modal v-model="isTaskModalVisible" title="Создание задачи" hide-footer>
+          <b-form @submit.prevent="tryAddTask">
+            <b-form-group label="Название">
+              <b-form-input
+                  name="NameTask"
+                  placeholder="Название задачи"
+                  v-model="model.title"
+              ></b-form-input>
+            </b-form-group>
+
+            <b-form-group label="Описание">
+              <b-form-input
+                  name="description"
+                  placeholder="Описание"
+                  v-model="model.description"
+              ></b-form-input>
+            </b-form-group>
+            
+            <b-form-group label="Выполнить до">
+              <input type="date" v-model="model.deadline"/>
+            </b-form-group>
+          </b-form>
+
+          <b-form-group label="Статус задачи">
             <b-form-select v-model="model.taskStatus" :options="statusOptions" required></b-form-select>
-          </div>
-          <div>
-            <label for="assignedUser">Исполнитель:</label>
-            <b-form-select v-model="model.assignedUser.id" :options="userOptions" required></b-form-select>
-          </div>
-          <b-button @click="tryAddTask">Добавить задачу</b-button>
+          </b-form-group>
+
+          <b-form-group label="Задать исполнитея">
+            <b-form-select v-model="model.assignedUser" :options="userOptions" required></b-form-select>
+          </b-form-group>
+          <b-button class="addTaskButton w-30" type="submit" block>Добавить задачу</b-button>
+
         </b-modal>
       </div>
     </form>
 
     <b-collapse v-model="isTaskListVisible" class="mt-3">
       <b-card>
-        <ul class="list-group">
+        <ul v-if="tasks.length > 0" class="list-group">
           <li v-for="task in tasks" :key="task.id" class="list-group-item d-flex justify-content-between align-items-center">
             <div>
               <h5>{{ task.title }}</h5>
@@ -44,6 +54,16 @@
             <b-button variant="outline-danger" @click="deleteTask(task.id)">Удалить</b-button>
           </li>
         </ul>
+        <b-card v-else>
+          <div>
+            <h5>{{ defaultTask.title }}</h5>
+            <p>{{ defaultTask.description }}</p>
+            <p><strong>Статус:</strong> {{ defaultTask.taskStatus }}</p>
+            <p><strong>Дедлайн:</strong> {{ defaultTask.deadline }}</p>
+            <p><strong>Исполнитель:</strong> {{ defaultTask.assignedUser.username }}</p>
+          </div>
+          <b-button variant="outline-danger" @click="deleteTask(task.id)">Удалить</b-button>
+        </b-card>
       </b-card>
     </b-collapse>
   </div>
@@ -77,7 +97,14 @@ export default {
         { value: 'Необходимо выполнить', text: 'Необходимо выполнить' },
         { value: 'Срочно', text: 'Срочно' }
       ],
-      userOptions: []
+      userOptions: [],
+      defaultTask: {
+        title: 'Тестовая',
+        description: 'Тест Описание',
+        taskStatus: 'Новое',
+        deadline: '01.01.1999',
+        assignedUser: { id: 0, username: 'Пользователь', password: '' }
+      }
     }
   },
   methods: {
@@ -136,5 +163,15 @@ export default {
 </script>
 
 <style scoped>
-
+.task-manager{
+  font-family: Natural Mono Alt Regular;
+  
+}
+.task-manager{
+  background-color: #355957
+}
+.addTaskButton{
+  background-color: #355957;
+  border-radius: 10px;
+}
 </style>
